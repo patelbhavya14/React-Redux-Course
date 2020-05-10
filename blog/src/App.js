9e265fb68b2b0f608ee6aecd6905c5c8ce1c7161
Bhavya Patel <patel.bhavya@husky.neu.edu>
2020-05-10 18:39:55 -0400

    blog app - radux app number 2

---
+++ blog/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PostList from "./components/PostList";
+
+function App() {
+  return (
+    <div className="ui container">
+      <PostList />
+    </div>
+  );
+}
+
+export default App;
---
+++ blog/src/actions/index.js
@@ -0,0 +1,44 @@
+import _ from "lodash";
+import jsonPlaceholder from "../apis/jsonPlaceholder";
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  // const userIds = _.uniq(_.map(getState().posts, "userId"));
+
+  // userIds.forEach((id) => dispatch(fetchUser(id)));
+
+  _.chain(getState().posts)
+    .map("userId")
+    .uniq()
+    .forEach((id) => dispatch(fetchUser(id)))
+    .value();
+};
+
+export const fetchPosts = () => async (dispatch) => {
+  const res = await jsonPlaceholder.get("/posts");
+  dispatch({
+    type: "FETCH_POSTS",
+    payload: res.data,
+  });
+};
+
+export const fetchUser = (id) => async (dispatch) => {
+  const res = await jsonPlaceholder.get(`/users/${id}`);
+  dispatch({
+    type: "FETCH_USER",
+    payload: res.data,
+  });
+};
+
+// export const fetchUser = (id) => (dispatch) => {
+//   _fetchUser(id, dispatch);
+// };
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const res = await jsonPlaceholder.get(`/users/${id}`);
+//   dispatch({
+//     type: "FETCH_USER",
+//     payload: res.data,
+//   });
+// });
